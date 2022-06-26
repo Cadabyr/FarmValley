1c2b09e35727e6da4ca97c4c39566a0be2132120
Chris Stump <ChrisMichaelStump@gmail.com>
2022-06-26 14:03:11 -0400

    Fix dirt application and give radius to placement

--- Assets/FarmValley/Player/PlayerDigAction.cs
+++ Assets/FarmValley/Player/PlayerDigAction.cs
@@ -20,20 +20,39 @@ namespace FarmValley.Player
         {
             if (Input.GetMouseButtonDown(0))
             {
-                Vector3 mousePos = Input.mousePosition;
-                Vector3 objectPosition = Camera.main.ScreenToWorldPoint(mousePos);
+                Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector2 worldMousePosition2D = new Vector2(worldMousePosition.x, worldMousePosition.y);
+
+                var distance = Vector2.Distance(worldMousePosition2D, transform.position);
+
+                if(distance > 2)
+                {
+                    return;
+                }
+
+                Vector3 objectPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 objectPosition.z = 0;
 
-                RaycastHit2D hit = Physics2D.Raycast(objectPosition, -Vector2.up);
+                objectPosition.x = Mathf.Floor(objectPosition.x) + .5f;
+                objectPosition.y = Mathf.Floor(objectPosition.y) + .5f;
+
+                int layerMask = ~(LayerMask.GetMask("Player"));
+
+                RaycastHit2D hit = Physics2D.Raycast(objectPosition, Vector2.zero, 1, layerMask);
                 
                 if(hit.collider != null)
                 {
-                    Destroy(hit.collider.gameObject);
-                    return;
+                    if(hit.collider.gameObject.CompareTag("Dirt"))
+                    {
+                        Destroy(hit.collider.gameObject);
+                        return;
+                    }
                 }
 
                 GameObject dirtObject = Instantiate(dirt);
                 dirtObject.transform.position = objectPosition;
+
+                Physics2D.IgnoreCollision(dirtObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
             }
         }
     }
